b483a4ddedad879c0d8237209ba5b6359fc7116e
1E10 Laptop <sample@bths.edu>
2022-09-21 12:20:43 -0400

    //I am practicing committing (Doing the bare minimum)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld
+
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Doing the bare minimum");
+    }
 }
